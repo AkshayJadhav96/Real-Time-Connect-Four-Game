63a1f5422010331095ef7edcd5479cd49071edb3
Akshay <akshayjadhavkrishnaholic@gmail.com>
2025-08-31 01:38:22 +0530

    refactoring

--- backend/src/connect_four_game/main.py
+++ backend/src/connect_four_game/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import HTMLResponse
 import uuid
-from game_logic import Game
+from backend.src.connect_four_game.game_logic import Game
 import json
 import logging
 import asyncio # <--- 1. IMPORT ASYNCIO
